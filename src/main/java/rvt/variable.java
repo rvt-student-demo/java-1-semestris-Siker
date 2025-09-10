1d48d7a9235a76f69730524a19230f4562e237dc
Ilja Judins <a240306IJ@rvt.lv>
2025-09-10 13:08:45 +0300

    variables

---
+++ src/main/java/rvt/variable.java
@@ -0,0 +1,30 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class variable {
+    
+    public static void main(String[] args) {
+
+        int chicken = 9000;
+        double bacon = 0.1;
+        String tractor = "Zetor";
+
+        
+
+        System.out.println("Chicken: \n" + chicken);
+        System.out.println("Bacon (kg): \n" + bacon);
+        System.out.println("Tractor: \n" + tractor + "\n");
+
+        System.out.println("And finally, a summary:");
+        System.out.println(chicken);
+        System.out.println(bacon);
+        System.out.println(tractor);
+
+
+    }
+
+
+
+
+}
